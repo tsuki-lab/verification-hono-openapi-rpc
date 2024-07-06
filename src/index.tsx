afae0fec0b6ad21168875cd4bb497a1fb46e3bca
hanetsuki <48175599+tsuki-lab@users.noreply.github.com>
2024-07-06 22:31:59 +0900

    chore: remove dependencies and files.

--- src/index.tsx
+++ src/index.tsx
@@ -1,31 +1,53 @@
+import { hc } from "hono/client";
 import { renderer } from "./renderer";
-import { api } from "./api";
-import { OpenAPIHono } from "@hono/zod-openapi";
-import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
+
+const userSchema = z
+  .object({
+    id: z.string().openapi({ description: "userId", example: "1" }),
+    name: z.string().openapi({ description: "userName", example: "John" }),
+  })
+  .openapi("User");
 
 const app = new OpenAPIHono();
 
-app.route("/api", api);
+const routes = app.openapi(
+  createRoute({
+    method: "get",
+    path: "/api/users/{id}",
+    request: {
+      params: z.object({
+        id: z.coerce.number().openapi({ description: "userId", example: 1 }),
+      }),
+    },
+    responses: {
+      200: {
+        content: {
+          "application/json": {
+            schema: userSchema,
+          },
+        },
+        description: "Get a user",
+      },
+    },
+  }),
+  (c) => {
+    return c.json({ id: "1", name: "John" });
+  }
+);
+
+const client = hc<typeof routes>("/");
+// The type does not match the intended shape
+// const client: {
+//   [x: string]: ClientRequest<{
+//       [Method: `$${Lowercase<string>}`]: Endpoint;
+//   }>;
+// }
 
 app.use(renderer);
 
 app.get("/", (c) => {
   return c.render(<h1>Hello!</h1>);
-});
-
-app.get(
-  "/swagger-ui",
-  swaggerUI({
-    url: "/openapi.json",
-  })
-);
-
-app.doc("/openapi.json", {
-  openapi: "3.0.0",
-  info: {
-    title: "API",
-    version: "1.0.0",
-  },
 });
 
 export default app;
